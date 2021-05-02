8c2ef5904b2f3040cd873c614a6f2937f6d2dc84
Manish <yourmanishsingh@gmail.com>
2021-05-02 14:10:22 +0530

    js changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import data from './data/word.json';
 
 function App() {
-  const wordArray = data.words || [];
+  const wordArray = data.words;
   const [word, setWord] = useState('');
-
-  const getRandomWord = () => wordArray[Math.floor(Math.random() * wordArray.length)];
-  const setRandomWord = () => setWord(getRandomWord());
+  const getRandomN = () => Math.floor(Math.random() * wordArray.length);
+  const setRandomWord = () => {
+    let index = getRandomN();
+    setWord(wordArray[index]);
+    wordArray.splice(index, 1);
+  }
 
   useEffect(() => {
     setRandomWord()
@@ -19,10 +22,10 @@ function App() {
         <h1>recall the word</h1>
       </header>
       <section className="App-body">
-        <h2>{word}</h2>
+        {word ? <h2>{word}</h2> : <span className="noWord">No word is remaining, <br /> please refresh the page.</span>}
       </section>
       <footer className="App-footer">
-        <button onClick={setRandomWord}>Next Word</button>
+        {word && <button onClick={setRandomWord}>Next Word</button>}
       </footer>
     </div>
   );
